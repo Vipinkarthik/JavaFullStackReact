c2d78aaad32fd56e10fc4b7a35da6baddcedfb87
Vipin karthik M B <vipinkarthik.mb2023ece@sece.ac.in>
2025-07-15 23:44:50 +0530

    Day11-JavaFullStackIntern

---
+++ Day11-ReactAppIntern/day11-reactviteapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+
+function App() {
+  return(
+    <div>
+      <SignUp />
+      <Login />
+    </div> 
+
+  )
+  
+}
+
+export default App
